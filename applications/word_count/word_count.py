150dff67b20c9c5c66c85fc75d0b552517028457
Jason <jlong5795@gmail.com>
2020-06-07 18:57:42 -0500

    did some more

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,30 @@
 def word_count(s):
-    # Your code here
+    string = s.lower().split(' ')
+    ignore = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&', '\\', ' ']
+    solution = {}
+    
+    for c in s:
+        if c in ignore:
+            s = s.replace(c, ' ')
 
+    # join the words back together, then remove spaces created by replacment, then split works on the spaces
+    s = ' '.join(s.split())
 
+    # makes string a list
+    new_string = s.split()
+
+    for c in new_string:
+        c = c.lower() #ignore case
+        if c in solution:
+            solution[c] += 1
+        else:
+            solution[c] = 1
+
+    return solution
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('Hello, my cat.  And my cat doesn\'t say "hello" back.'))
